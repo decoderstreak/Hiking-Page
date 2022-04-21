ca23a28d245cdb6defab0a6ca4da8858d150c6ce
decoderstreak <uman.tab12@gmail.com>
2022-04-21 12:45:52 +0530

    section 4

--- src/comp/Section4/Section4.js
+++ src/comp/Section4/Section4.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import "./Section4.css";
+import TravelAdvisor from '../../assets/icons/Travel Advisor.svg'
+import  UnitedTravel from '../../assets/icons/United Travel.svg'
+import TravelChannel from '../../assets/icons/Travel Channel.svg'
+import TripRaja from '../../assets/icons/tripraja.svg'
+import BookingCom from '../../assets/icons/Booking.svg'
 // import Section2_image from "../../assets/images/section6.png";
-
 
 export default class Section4 extends Component {
   render() {
@@ -12,15 +16,28 @@ export default class Section4 extends Component {
                 <div className="Section4-text1">Safe Journey</div>
                 <div className="Section4-text2">Trusted by Big Companies</div>
             </div>
-            <div className="Section4-part-2">
-            {/* Images in a row need to be displayed */}
-
-
+        
+             <div className="Section4-bottom-container-1">
+            <div><img src={TravelAdvisor}></img></div>
+            <div><img src={UnitedTravel}></img></div>
+           <div> <img src={TravelChannel}></img></div>
             </div>
+            
+            <div className="Section4-bottom-container-2">
+            <img src={TripRaja}></img>
+             <img src={BookingCom}></img>
+            </div>
+       
+              
+            </div>
+
+          </div>
+
+
+          
      
           
-        </div>
-      </div>
+      
     );
   }
 }
